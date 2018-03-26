dc7aae1ccbe32760eac6a2db6c528e3664fba5bb
yonathan Kebe <yonathankebe@yonathans-MacBook-Pro.local>
2018-03-26 19:03:10 -0400

    OdosII-026: Microservice gateway created using jhipster

---
+++ ODOSII-API/src/main/java/com/teamalpha/odos/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.teamalpha.odos.repository;
+
+import com.teamalpha.odos.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
